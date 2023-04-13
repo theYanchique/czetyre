0d0971b7f2fddd068c031edbd8f4b0ad71936404
theYanchique <przegranym@bk.ru>
2023-04-13 22:25:36 +0400

    3.0

---
+++ app/src/main/java/com/example/czetyre/LoginActivity.java
@@ -0,0 +1,51 @@
+package com.example.czetyre;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends Activity implements View.OnClickListener, GetData.AsyncResponse {
+
+    private EditText etLogin;
+    private EditText etPass;
+    private Button btnSubmit;
+    private String login, password, url_link;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etLogin=findViewById(R.id.eTLogin);
+        etPass=findViewById(R.id.eTPass);
+        btnSubmit=findViewById(R.id.btnSend);
+        btnSubmit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        login=etLogin.getText().toString();
+        password=etPass.getText().toString();
+        url_link="http://a0805137.xsph.ru/scriptUserProfile.php";
+        new GetData(this).execute(login, password, url_link);
+    }
+    @Override
+    public void processFinish(String result){
+        if(!result.equals("OSHIBKA")){
+            Intent intent=new Intent();
+            intent.putExtra(MainActivity.FULLUSERNAME, result);
+            setResult(RESULT_OK, intent);
+            finish();
+        } else {
+            Intent intent=new Intent();
+            setResult(RESULT_CANCELED, intent);
+            finish();
+        }
+    }
+}
--- app/src/main/java/com/example/czetyre/MainActivity.java
+++ app/src/main/java/com/example/czetyre/MainActivity.java
@@ -1,47 +1,72 @@
 package com.example.czetyre;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, GetData.AsyncResponse{
+import android.widget.TextView;
+import android.widget.Toast;
 
-    private EditText etLogin;
-    private EditText etPass;
-    private Button btnSubmit;
-    private String login, password, url_link;
+public class MainActivity extends AppCompatActivity {
+    private TextView tv;
+    private SharedPreferences sPref;
+    public static String MY_PREF="CZETYRE_PREFERENCES_FILE";
+    public final static String FULLUSERNAME="fullusername";
+    private int code=0;
+    private Toast toast;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        etLogin=findViewById(R.id.eTLogin);
-        etPass=findViewById(R.id.eTPass);
-        btnSubmit=findViewById(R.id.btnSend);
-        btnSubmit.setOnClickListener(this);
+        tv=findViewById(R.id.tvViewFullName);
+        String fullUserName;
+        if((fullUserName=loadUserFullNameFromMyPref())==null){
+            getUserFullNameFromLoginActivity();
+        }else {
+            tv.setText("Priwetstwuiu "+fullUserName);
+        }
+    }
+    private String loadUserFullNameFromMyPref(){
+        sPref=getApplicationContext().getSharedPreferences(MY_PREF, MODE_PRIVATE);
+        String fullUserName=sPref.getString(FULLUSERNAME, "");
+        if (fullUserName.isEmpty()){
+            return null;
+        }else {
+            return fullUserName;
+        }
+    }
+    private void getUserFullNameFromLoginActivity(){
+        Intent intent=new Intent(MainActivity.this, LoginActivity.class);
+        startActivityForResult(intent, code);
     }
 
     @Override
-    public void onClick(View v) {
-        login=etLogin.getText().toString();
-        password=etPass.getText().toString();
-        url_link="http://a0805137.xsph.ru/scriptUserProfile.php";
-
-        new GetData(this).execute(login, password, url_link);
-
-        /*Intent intent=new Intent(this, ViewActivity.class);
-        intent.putExtra("login",etLogin.getText().toString());
-        intent.putExtra("password",etPass.getText().toString());
-        startActivity(intent);*/
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==code){
+            if (resultCode==RESULT_OK){
+                String fullUserName=data.getStringExtra(FULLUSERNAME);
+                saveFullUserNameInMyPref(fullUserName);
+                tv.setText("Privetstwuiu "+fullUserName);
+            }else{
+                int duration= Toast.LENGTH_LONG;
+                if(toast!=null){
+                    toast.cancel();
+                }
+                toast=Toast.makeText(this,"TEBE ZDES NE RADY", duration);
+                toast.show();
+                getUserFullNameFromLoginActivity();
+            }
+        }
     }
-    @Override
-    public void processFinish(String output){
-        Intent intent=new Intent(this, ViewActivity.class);
-        intent.putExtra("full_name", output);
-        startActivity(intent);
+    private void saveFullUserNameInMyPref(String data){
+        sPref=getApplicationContext().getSharedPreferences(MY_PREF, MODE_PRIVATE);
+        SharedPreferences.Editor ed=sPref.edit();
+        ed.putString(FULLUSERNAME, data);
+        ed.commit();
     }
 }
